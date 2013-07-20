3ec57fd35ff4afb87e70859a9488d917ac8f67d5
Thomas Butt <tom.butt@gmail.com>
2013-07-19 23:43:40 -0600

    quick sns attempt, anticipating issues

--- routes/email.js
+++ routes/email.js
@@ -1,6 +1,26 @@
 /**
  * New node file
  */
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./creds.json');
+
 exports.sendsqs = function(req, res){
 	res.send("this will send an sqs");
+};
+
+exports.sendsns = function(req, res){
+	res.send("this will send an sns");
+	var sns = new AWS.SNS();
+	sns.publish({
+		"TopicArn":"arn:aws:sns:us-east-1:047247006121:nodeemail",
+		"Message":req.body
+	}, function(err,data){
+		if (err){
+			console.log("ERR: "+err);
+		}
+	});
+};
+
+exports.sendnode = function(req, res){
+	res.send("this will send email from node directly");
 };
