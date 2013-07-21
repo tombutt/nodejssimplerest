7d49c6a638628f6dfc28c72c9d6d49e58449c40f
root <root@ip-10-137-49-19.ec2.internal>
2013-07-21 04:54:18 +0000

    some updates to the SES code

--- routes/email.js
+++ routes/email.js
@@ -4,6 +4,24 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('/usr/local/node/nodejssimplerest/creds.json');
 
+exports.sendses = function(req,res){
+	res.send("this will send via SES");
+	var ses = new AWS.SES();
+	ses.sendEmail({
+//JSON PARAMS
+	"Source":"info@cheekyware.com",
+	"Destination":{"ToAddresses":["info@cheekyware.com"]
+		},
+	"Message":{"Subject":{"Data":"SES message"},
+	"Body":{"Text":{"Data":JSON.stringify(req.body)}}
+		}
+	}, function(err,data){
+		if (err){
+			console.log("ERR: "+err);
+		}
+//RESULT FUNCTION
+	});
+}
 exports.sendsqs = function(req, res){
 	res.send("this will send an sqs");
 };
